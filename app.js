64d71ec31b44c593f8bccddee275ba535fbaefa6
luciof5 <alex.ribeiro@ef5.com.br>
2019-08-02 13:19:34 -0300

    add express

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var http = require('http');
+const fetch = require('node-fetch');
+const url = require('url');
+const rendertron = require('rendertron-middleware');
+var app = express();
+
+app.use(rendertron.makeMiddleware({
+    proxyUrl: 'https://fair-terminus-245617.appspot.com/render',
+}));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('*', express.static(path.join(__dirname, 'dist')));
+
+module.exports = app;
